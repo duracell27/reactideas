48972d08596ce83662bff0f10c6a567d94226748
duracell27 <volodymyrshmidt@gmail.com>
2019-11-07 10:34:11 +0200

    add some redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import allReducers from './reducers';
 
+const store = createStore(allReducers);
+ 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/idea.js
@@ -0,0 +1,28 @@
+export default function(){
+    return[
+        {
+            id: 1,
+            name: 'lorem ipsum 1',
+            description: 'Lorem lorem lorem lorem lorem ipsum sit amet dolor',
+            author: 'admin',
+            like: 6,
+            dislike: 2,
+        },
+        {
+            id: 2,
+            name: 'lorem ipsum 2',
+            description: 'Lorem lorem lorem lorem lorem ipsum sit amet dolor',
+            author: 'admin',
+            like: 11,
+            dislike: 0,
+        },
+        {
+            id: 3,
+            name: 'lorem ipsum 3',
+            description: 'Lorem lorem lorem lorem lorem ipsum sit amet dolor',
+            author: 'admin',
+            like: 6,
+            dislike: 15,
+        },
+    ]
+}
